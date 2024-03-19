cb62c96007f03b04bd7803847dbbc1d94062f9fb
fatihozcandev <fatih-ozcan@windowslive.com>
2024-03-19 19:36:47 +0100

    date-fn implemented

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { tr } from "date-fns/locale";
+import { differenceInDays, formatDistanceToNow } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
+  const deadline = new Date(taskObj.deadline);
+  const deadLineInfo = formatDistanceToNow(deadline, {
+    locale: tr,
+    addSuffix: true,
+  });
+
+  const terminClass =
+    differenceInDays(deadline, new Date()) < 3 ? "bg-urgent" : "bg-normal";
   return (
     <div className="task p-6 bg-white rounded-md leading-normal mt-4 shadow-[0_4px_5px_0_rgb(0 0 0 / 10%)]">
       <h3 className="text-lg text-[#c8781a]">{taskObj.title}</h3>
       <div className="deadline text-xs pt-1">
         son teslim:{" "}
-        <span className="py-1 px-2 rounded-sm inline-block">
-          {taskObj.deadline}
+        <span className={` ${terminClass} py-1 px-2 rounded-sm inline-block`}>
+          {deadLineInfo}
         </span>
       </div>
       <p className="pt-2 pb-3 text-[#444] text-sm">{taskObj.description}</p>
